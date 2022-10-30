d80b09af4e4b952b1d5e4af474957c8d7f5c43e5
Daniel Marzini <44803752+danielmarzini@users.noreply.github.com>
2022-10-30 22:07:28 +0100

    Update main.go

--- internal/metrics/main.go
+++ internal/metrics/main.go
@@ -4,33 +4,44 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/demizer/go-logs/src/logs"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/restmapper"
 
+	informers "k8s.io/client-go/informers/core/v1"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-func createKubeClient(kubeConfigPath *string) *kubernetes.Clientset {
+// creataKubeDynamicClient()
+func createKubeDynamicClient(kubeConfigPath *string) (dynamic.Interface, error) {
 	var config *rest.Config
 	var err error
 
-	// try in-cluster config, and then default to kubeconfig
 	if config, err = rest.InClusterConfig(); err != nil {
 		config, err = clientcmd.BuildConfigFromFlags("", *kubeConfigPath)
 		if err != nil {
 			logs.Criticalf("failed parsing kubeconfig %v", err.Error())
+			return nil, err
 		}
 	}
 
-	clientset, err := kubernetes.NewForConfig(config)
+	dynClient, err := dynamic.NewForConfig(config)
 	if err != nil {
-		logs.Criticalf("failed creating the clientset %v", err.Error())
+		return nil, err
 	}
-	return clientset
+
+	return dynClient, nil
 }
 
 func getKubeConfigPath() *string {
@@ -44,14 +55,97 @@ func getKubeConfigPath() *string {
 	return kubeconfigPath
 }
 
-func main() {
-	var defaultNamespace = flag.String("namespace", "kube-system", "namespace to use for k8s")
-	client := createKubeClient(getKubeConfigPath())
-	podList, err := client.CoreV1().Pods(*defaultNamespace).List(context.Background(), metav1.ListOptions{})
+func podsCounterTest(client *kubernetes.Clientset, namespace string) {
+	podList, err := client.CoreV1().Pods(namespace).List(context.Background(), metav1.ListOptions{})
 	if err != nil {
 		logs.Criticalf("Failed retrieving pods %v", err)
 	}
 	for _, pod := range podList.Items {
 		logs.Printf("Pod %s \n", pod.Name)
 	}
+}
+
+func podsInformerTest(client *kubernetes.Clientset, namespace string) {
+	podSharedInformer := informers.NewPodInformer(client, namespace, 0, cache.Indexers{})
+	pods := []*v1.Pod{}
+	for _, p := range podSharedInformer.GetStore().List() {
+		p := p.(*v1.Pod)
+		logs.Println(p)
+		pods = append(pods, p)
+	}
+	logs.Printf("pods: %d", len(pods))
+}
+
+var podResource = schema.GroupVersionResource{
+	Group:    "",
+	Version:  "v1",
+	Resource: "pods",
+}
+
+func ListPods(ctx context.Context, client dynamic.Interface, namespace string) ([]unstructured.Unstructured, error) {
+	list, err := client.Resource(podResource).Namespace(namespace).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		logs.Criticalln(err)
+		return nil, err
+	}
+	for _, pod := range list.Items {
+		logs.Println(pod.GetName())
+	}
+	return list.Items, nil
+}
+
+func countResources(ctx context.Context, client dynamic.Interface, namespace string) (int, error) {
+	var counter int
+
+	list, err := client.Resource(schema.GroupVersionResource{
+		Group:    "",
+		Version:  "v1",
+		Resource: "pods",
+	}).Namespace(namespace).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		logs.Criticalln(err)
+		return 0, err
+	}
+	counter = len(list.Items)
+	return counter, nil
+}
+
+func findResources(kubeconfigPath *string) {
+	kubeconfig, _ := ioutil.ReadFile(*kubeconfigPath)
+	restconfig, err := clientcmd.RESTConfigFromKubeConfig(kubeconfig)
+	if err != nil {
+		logs.Criticalln(err)
+		os.Exit(1)
+	}
+	dc := discovery.NewDiscoveryClientForConfigOrDie(restconfig)
+	groupResources, err := restmapper.GetAPIGroupResources(dc)
+	if err != nil {
+		logs.Criticalln(err)
+		os.Exit(1)
+	}
+	mapper := restmapper.NewDiscoveryRESTMapper(groupResources)
+	mapping, err := mapper.RESTMapping(schema.ParseGroupKind("Deployment.apps"), "v1")
+	if err != nil {
+		logs.Criticalln(err)
+		os.Exit(1)
+	}
+	fmt.Println(mapping.Resource)
+}
+
+// out of band CRDs
+
+func main() {
+	// ctx := context.Background()
+	// client, err := createKubeDynamicClient(getKubeConfigPath())
+	// if err != nil {
+	// 	logs.Criticalln(err)
+	// 	os.Exit(1)
+	// }
+	// size, err := countResources(ctx, client, "")
+	// if err != nil {
+	// 	logs.Criticalln(err)
+	// 	os.Exit(1)
+	// }
+	// logs.Printf("Objects found: %d\n", size)
+	findResources(getKubeConfigPath())
 }
